27aab29e28f0eb91b071162b9c0e21f9cb146fef
hs <sgavrilyn@gmail.com>
2023-01-02 22:18:01 +0200

    add user urls fix same bugs

--- user/views.py
+++ user/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from user.models import User
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    users = User.objects.all()
+    res = '<h1>All users</h1>'
+    for item in users:
+        res += f'<div><p>{item.first_name}</p></div>'
+    return HttpResponse(res)
